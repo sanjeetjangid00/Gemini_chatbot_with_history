a0136502d17cfa33c8f746638096f2bafc6ee949
SANJEET <167337847+sanjeetjangid00@users.noreply.github.com>
2025-01-08 11:30:54 +0530

    Update Gemini_chatbot_with_chat_history.py

--- Gemini_chatbot_with_chat_history.py
+++ Gemini_chatbot_with_chat_history.py
@@ -40,10 +40,10 @@ def stream_data(response):
     for word in response.split(" "):
         yield word + " "
         time.sleep(0.02)
-template = "You are a conversational chatbot created by Sanjeet Jangid.you have to give response of user chats only and next sentence is user's chat."
+        
 chat = st.chat_input("Enter your message:")
 if chat:
-    st.session_state["chat_history"].append(HumanMessage(template + str(chat)))
+    st.session_state["chat_history"].append(HumanMessage(str(chat)))
 
     # Prepare the message state
     state = MessagesState(messages=st.session_state["chat_history"])
